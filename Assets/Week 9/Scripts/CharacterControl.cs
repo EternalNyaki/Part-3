1f2c65c39b1ca4bc86c2cdf2b1c4547b19644152
MSI\etern <reyjoshu@sheridancollege.ca>
2024-03-14 13:16:14 -0400

    Task 4b: Made UI text display type of currently selected villager

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI villagerTypeText;
+
     public static Villager SelectedVillager { get; private set; }
+
+    private void Update()
+    {
+        if(SelectedVillager == null)
+        {
+            villagerTypeText.text = "No Villager Selected";
+        }
+        else
+        {
+            villagerTypeText.text = SelectedVillager.GetType().Name;
+        }
+    }
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
