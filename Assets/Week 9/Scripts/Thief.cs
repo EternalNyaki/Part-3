52ad9fb4fe989ec561aa4eaa51207d997dcdb837
MSI\etern <reyjoshu@sheridancollege.ca>
2024-03-21 09:58:53 -0400

    Task 1: Dash with a coroutine

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -1,25 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Thief : Villager
 {
-    public float dashDistance;
-
     public GameObject daggerPrefab;
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    Coroutine dashing;
 
     protected override void Attack()
     {
-        float targetRadians = Vector2.SignedAngle(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) * Mathf.Deg2Rad;
-        Vector2 deltaPosition = new Vector2(Mathf.Cos(targetRadians), Mathf.Sin(targetRadians)) * dashDistance;
-        transform.Translate(deltaPosition);
-        base.destination = transform.position;
+        if(dashing != null)
+        {
+            StopCoroutine(dashing);
+        }
+        dashing = StartCoroutine(Dash());
+    }
+
+    private IEnumerator Dash()
+    {
+
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        speed = 7f;
+
+        while(speed > 3)
+        {
+            yield return null;
+        }
 
         base.Attack();
 
+        yield return new WaitForSeconds(9/60);
         Instantiate(daggerPrefab, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(13/60);
         Instantiate(daggerPrefab, spawnPoint2.position, spawnPoint2.rotation);
     }
 
